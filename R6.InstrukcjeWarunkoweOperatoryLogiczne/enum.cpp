36a2650e998d0927e30291bb90999123dcf16dda
Adam-Czerwinski <adam.czerwinski097@gmail.com>
2018-10-24 20:12:34 +0200

    Skonczony plik enum.cpp

--- R6.InstrukcjeWarunkoweOperatoryLogiczne/enum.cpp
+++ R6.InstrukcjeWarunkoweOperatoryLogiczne/enum.cpp
@@ -16,10 +16,30 @@ enum {
 int main()
 {
   SetConsoleCP(1250);
-  SetConsoleOutputCP(1250);
-  setlocale(LC_ALL,".1250");
+	SetConsoleOutputCP(1250);
+	setlocale(LC_ALL, ".1250");
 
-  
+  cout<<"Podaj kod koloru (0-6): ";
+  int  code;
+  cin>>code;
+
+  while(code>=red && code<=indigo)
+  {
+    switch(code)
+    {
+      case red: cout<<"Jej usta były czerwone.\n"; break;
+      case orange: cout<<"Jej usta były orange.\n"; break;
+      case yellow: cout<<"Jej usta były yellow.\n"; break;
+      case green: cout<<"Jej usta były green.\n"; break;
+      case blue: cout<<"Jej usta były blue.\n"; break;
+      case violet: cout<<"Jej usta były violet.\n"; break;
+      case indigo: cout<<"Jej usta były indigo.\n"; break;
+    }
+    cout<<"Podaj kod koloru (0-6): ";
+    cin>>code;
+  }
+  cout<<"Do widzenia!\n";
+
 
   return 0;
 }
