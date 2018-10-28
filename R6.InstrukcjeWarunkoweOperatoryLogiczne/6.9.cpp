b80d3f17910249e2233ad5f7c11da05063e9f7db
Adam-Czerwinski <adam.czerwinski097@gmail.com>
2018-10-28 18:55:40 +0100

    Added 6.9 exercise

---
+++ R6.InstrukcjeWarunkoweOperatoryLogiczne/6.9.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int NAZWISKO_SIZE=30;
+
+struct wplacajacy{
+  char nazwisko[NAZWISKO_SIZE];
+  double kwota;
+};
+
+int main()
+{
+
+
+  ifstream inFile;
+  inFile.open("6.9.txt");
+
+  int liczbaWplacajacych;
+  inFile>>liczbaWplacajacych;
+  inFile.get();
+
+  wplacajacy *wplacajacyTab = new wplacajacy[liczbaWplacajacych];
+  int linia=1;
+  int i=0;
+
+  //ALGORYTM
+  while(!inFile.eof())
+  {
+    switch(linia)
+    {
+      case 1:
+        inFile.getline((*(wplacajacyTab+i)).nazwisko,NAZWISKO_SIZE);
+        linia++;
+        break;
+
+      case 2:
+        inFile>>(*(wplacajacyTab+i)).kwota;
+        inFile.get();
+        linia=1;
+        i++;
+        break;
+    }
+  }
+
+  //WYSWIETLANIE
+  cout<<"Nasi Wspaniali Sponsorzy\n";
+  int iloscWspanialychSponsorow{};
+  for(int i=0; i<liczbaWplacajacych;i++)
+  {
+    if((wplacajacyTab+i)->kwota>=10000.0)
+    {
+      cout<<(wplacajacyTab+i)->nazwisko<<", ";
+      cout<<(wplacajacyTab+i)->kwota<<endl;
+      iloscWspanialychSponsorow++;
+    }
+  }
+  if(!iloscWspanialychSponsorow)
+    cout<<"Brak\n";
+
+  cout<<"Nasi Sponsorzy\n";
+  int iloscSponsorow{};
+  for(int i=0; i<liczbaWplacajacych;i++)
+  {
+    if((wplacajacyTab+i)->kwota<10000.0)
+    {
+      cout<<(wplacajacyTab+i)->nazwisko<<", ";
+      cout<<(wplacajacyTab+i)->kwota<<endl;
+      iloscSponsorow++;
+    }
+  }
+  if(!iloscSponsorow)
+    cout<<"Brak\n";
+
+  delete wplacajacyTab;
+  return 0;
+}
